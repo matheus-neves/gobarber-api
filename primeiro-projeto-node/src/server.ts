079a97ce1b19b9e6edde149f1dee87fc69037c7f
Matheus Neves <matheusneves1989@gmail.com>
2020-04-13 17:34:36 -0300

    Estrutura e padrões - EsLint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello GoStack!' });
-})
+app.get('/', (req, res) => res.json({ message: 'Hello GoStack!' }));
 
 app.listen(3333, () => {
   console.log('🚀Server started on port 3333!');
-})
+});
