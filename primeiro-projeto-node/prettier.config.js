22d6e4ef9668e6652f9c56c55745346a790c5879
Matheus Neves <matheusneves1989@gmail.com>
2020-04-13 18:11:01 -0300

    Estrutura e padrões - Prettier

---
+++ primeiro-projeto-node/prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples
+  trailingComma: 'all', // virgula ao final de objetos
+  arrowParens: 'avoid' // arrow function sem barras, parametro unico
+}
