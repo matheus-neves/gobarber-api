cb438e35d275c089ec482c9483d01529e4726ca0
Matheus Neves <matheusneves1989@gmail.com>
2020-04-14 20:39:44 -0300

    Construindo Aplicação - Listando agendamentos

---
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,31 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointmentInSameDate = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointmentInSameDate || null;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRepository;
