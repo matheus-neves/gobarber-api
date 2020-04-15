9fa2fbb7b721b0418c5c9c2cb7637c0b00724308
Matheus Neves <matheusneves1989@gmail.com>
2020-04-14 21:09:46 -0300

    Construindo Aplicação - Trabalhando com dados

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,11 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentRepository {
   private appointments: Appointment[];
 
@@ -20,8 +25,11 @@ class AppointmentRepository {
     return findAppointmentInSameDate || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({
+      provider,
+      date,
+    });
     this.appointments.push(appointment);
 
     return appointment;
