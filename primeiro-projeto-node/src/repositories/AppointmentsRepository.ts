0303c72e3a8e67b91e5140a9c58a04f03601a4bd
Matheus Neves <matheusneves1989@gmail.com>
2020-04-18 21:12:21 -0300

    Banco de Dados - Repositório do TypeORM

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -1,39 +1,15 @@
-import { isEqual } from 'date-fns';
+import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '../models/Appointment';
 
-interface CreateAppointmentDTO {
-  provider: string;
-  date: Date;
-}
-
-class AppointmentRepository {
-  private appointments: Appointment[];
-
-  constructor() {
-    this.appointments = [];
-  }
-
-  public all(): Appointment[] {
-    return this.appointments;
-  }
-
-  public findByDate(date: Date): Appointment | null {
-    const findAppointmentInSameDate = this.appointments.find(appointment =>
-      isEqual(date, appointment.date),
-    );
-
-    return findAppointmentInSameDate || null;
-  }
-
-  public create({ provider, date }: CreateAppointmentDTO): Appointment {
-    const appointment = new Appointment({
-      provider,
-      date,
+@EntityRepository(Appointment)
+export default class AppointmentRepository extends Repository<Appointment> {
+  public async findByDate(date: Date): Promise<Appointment | null> {
+    const findAppointment = await this.findOne({
+      where: {
+        date,
+      },
     });
-    this.appointments.push(appointment);
 
-    return appointment;
+    return findAppointment || null;
   }
 }
-
-export default AppointmentRepository;
