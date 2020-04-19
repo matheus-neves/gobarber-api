f3624aa5704c444be4808274a6796e21a5e4f085
Matheus Neves <matheusneves1989@gmail.com>
2020-04-19 15:04:03 -0300

    Cadastro de Usuários - Criptografia de senha

--- primeiro-projeto-node/src/routes/users.routes.ts
+++ primeiro-projeto-node/src/routes/users.routes.ts
@@ -15,6 +15,8 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
+    delete user.password;
+
     return response.status(201).json(user);
   } catch (err) {
     return response.status(400).json({ message: err.message });
--- primeiro-projeto-node/src/services/CreateUserService.ts
+++ primeiro-projeto-node/src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,10 +20,12 @@ class CreateUserService {
       throw Error('Email already exists');
     }
 
+    const hashedPassword = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
 
     await usersRepository.save(user);
