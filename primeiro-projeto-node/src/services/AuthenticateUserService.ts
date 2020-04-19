454d21e31390493f1df4f06643bc5bd18460b3f2
Matheus Neves <matheusneves1989@gmail.com>
2020-04-19 16:40:38 -0300

    Autenticação - Gerando token JWT

--- primeiro-projeto-node/src/services/AuthenticateUserService.ts
+++ primeiro-projeto-node/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -7,8 +8,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const userRepository = getRepository(User);
 
     const user = await userRepository.findOne({
@@ -25,8 +31,14 @@ class AuthenticateUserService {
       throw Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'b3770151ea27ad599f05d9c65f8bad7e', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
