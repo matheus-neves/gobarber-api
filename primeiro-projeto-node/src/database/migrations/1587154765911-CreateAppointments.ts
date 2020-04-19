cea52ac8b8b3bf42e361337d1961a4f754c9c35a
Matheus Neves <matheusneves1989@gmail.com>
2020-04-19 11:44:26 -0300

    Cadastro de Usuários - Relacionamento nos models

--- primeiro-projeto-node/src/database/migrations/1587154765911-CreateAppointments.ts
+++ primeiro-projeto-node/src/database/migrations/1587154765911-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1587154765911
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- primeiro-projeto-node/src/models/Appointment.ts
+++ primeiro-projeto-node/src/models/Appointment.ts
@@ -4,15 +4,31 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
+
+/**
+ * Um para Um (OneToOne)
+ * Um para Muitos (OneToMany)
+ * Muitos para Muitos (ManyToMany)
+ */
 
 @Entity('appointments')
 export default class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column('')
-  provider: string;
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+  // Criar essa propriedade provider é importante para fins de consulta.
+  // Ela será uma instancia da classe User.
 
   @Column('timestamp with time zone')
   date: Date;
