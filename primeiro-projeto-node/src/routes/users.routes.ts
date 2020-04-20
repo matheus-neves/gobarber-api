b7a6c05408d52900748a9879afbe7871052b77ed
Matheus Neves <matheusneves1989@gmail.com>
2020-04-20 15:59:47 -0300

    Images upload - avatar updating

--- primeiro-projeto-node/src/routes/users.routes.ts
+++ primeiro-projeto-node/src/routes/users.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
@@ -32,8 +34,20 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+      });
+
+      delete user.password;
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ message: err.message });
+    }
   },
 );
 
---
+++ primeiro-projeto-node/src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,39 @@
+import { getRepository } from 'typeorm';
+import { join } from 'path';
+import { promises } from 'fs';
+import uploadConfig from '../config/upload';
+import User from '../models/User';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw Error('Only authenticated users can change avatar.');
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
