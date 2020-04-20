29671e2a16600165811b748bc30ac8df4f00fa5c
Matheus Neves <matheusneves1989@gmail.com>
2020-04-20 16:57:30 -0300

    Handling exceptions - Add class AppError

--- primeiro-projeto-node/src/routes/session.routes.ts
+++ primeiro-projeto-node/src/routes/session.routes.ts
@@ -4,22 +4,18 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response, next) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.json({ user, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ message: err.message });
-  }
+  return response.json({ user, token });
 });
 
 export default sessionsRouter;
