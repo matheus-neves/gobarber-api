a48c0c776b340b3289c7fab5090dc5bdcdce6c2a
Matheus Neves <matheusneves1989@gmail.com>
2020-05-13 18:00:10 -0300

    feat: generate user tokens

---
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -0,0 +1,21 @@
+import { uuid } from 'uuidv4';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserToken from '../../infra/typeorm/entities/UserToken';
+
+export default class FakeUserTokensRepository implements IUserTokensRepository {
+  private userTokens: UserToken[] = [];
+
+  public async generate(user_id: string): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      id: uuid(),
+      token: uuid(),
+      user_id,
+    });
+
+    this.userTokens.push(userToken);
+
+    return userToken;
+  }
+}
