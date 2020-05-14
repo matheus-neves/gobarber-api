552ef9b1a04c9d71081ff87d1c3abe7f91aed508
Matheus Neves <matheusneves1989@gmail.com>
2020-05-14 16:58:36 -0300

    feat: add findByToken

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -12,9 +12,19 @@ export default class FakeUserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
+
+    return userToken;
+  }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => token === findToken.token,
+    );
 
     return userToken;
   }
