9192de5c4767cc49dadfe3b766a7bad1542ca733
Matheus Neves <matheusneves1989@gmail.com>
2020-05-17 15:06:16 -0300

    feat: add dev mail provider

---
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -0,0 +1,32 @@
+import nodemailer, { Transporter } from 'nodemailer';
+import IMailProvider from '../models/IMailProvider';
+
+export default class EtherealMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'virgil.von@ethereal.email',
+        pass: 'wJ4bQgC2TESd1htjqY',
+      },
+    });
+
+    this.client = transporter;
+  }
+
+  public async sendMail(to: string, body: string): Promise<void> {
+    const message = await this.client.sendMail({
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
+      to,
+      subject: 'Recuperação de senha',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
