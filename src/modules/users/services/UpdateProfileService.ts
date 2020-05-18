f4624d34844f90ab5d28c17a8cbb4a50f548a55b
Matheus Neves <matheusneves1989@gmail.com>
2020-05-18 19:49:03 -0300

    feat: add update profile service

---
+++ src/modules/users/services/UpdateProfileService.ts
@@ -0,0 +1,76 @@
+import { inject, injectable } from 'tsyringe';
+// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
+import User from '../infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  user_id: string;
+  name: string;
+  email: string;
+  old_password?: string;
+  password?: string;
+}
+
+@injectable()
+class UpdateProfileService {
+  constructor(
+    @inject('UserRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError({ message: 'User not found' });
+    }
+
+    const userWithUpdatedEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user_id) {
+      throw new AppError({ message: 'E-mail already in use.' });
+    }
+
+    user.name = name;
+    user.email = email;
+
+    if (password && !old_password) {
+      throw new AppError({
+        message: 'You need to inform the old password to set a new password',
+      });
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+
+      if (!checkOldPassword) {
+        throw new AppError({
+          message: 'Old password does not match',
+        });
+      }
+
+      user.password = await this.hashProvider.generateHash(password);
+    }
+
+    this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateProfileService;
