53affe2e522d69c085002393d805ad03b826acb6
Matheus Neves <matheusneves1989@gmail.com>
2020-05-20 16:56:27 -0300

    style: comment removed

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -8,12 +8,6 @@ const appointmentController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', appointmentController.create);
 
 export default appointmentsRouter;
