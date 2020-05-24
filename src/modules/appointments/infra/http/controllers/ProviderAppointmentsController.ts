4b9aec5b2d142b07929f3125987b5f9a63c653f0
Matheus Neves <matheusneves1989@gmail.com>
2020-05-24 15:50:51 -0300

    feat(listprovider): add service from provider appointments | controller | route

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointments,
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}
