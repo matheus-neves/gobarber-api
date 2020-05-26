2ab7e4d8dc3ca8635f2a1791f3e371ab778ab720
Matheus Neves <matheusneves1989@gmail.com>
2020-05-26 18:35:02 -0300

    fix: joi method call, 'required()'

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -19,7 +19,7 @@ usersRouter.post(
     [Segments.BODY]: {
       name: Joi.string().required(),
       email: Joi.string().email().required(),
-      password: Joi.string().required,
+      password: Joi.string().required(),
     },
   }),
   usersController.create,
