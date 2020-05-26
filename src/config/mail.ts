46b6a0188ce50c0f8300072b6aba3721b97c4269
Matheus Neves <matheusneves1989@gmail.com>
2020-05-26 18:36:45 -0300

    feat(mail): add SES mail provider

---
+++ src/config/mail.ts
@@ -0,0 +1,20 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'gostack@matheus-neves.dev',
+      name: 'Matheus do GoStack',
+    },
+  },
+} as IMailConfig;
