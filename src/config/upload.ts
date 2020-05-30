a423a04d2df61a3443421629cc0af8b50e75a2de
Matheus Neves <matheusneves1989@gmail.com>
2020-05-30 16:43:05 -0300

    feat(storage): add s3 storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,48 @@
 import { resolve } from 'path';
 import { randomBytes } from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tmpFolder = resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tmpFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tmpFolder,
   uploadsFolder: resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename(req, file, cb) {
-      const fileHash = randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
-      return cb(null, fileName);
+
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(req, file, cb) {
+        const fileHash = randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+        return cb(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'appgobarber11',
     },
-  }),
-};
+  },
+} as IUploadConfig;
