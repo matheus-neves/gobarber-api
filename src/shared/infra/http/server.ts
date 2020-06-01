c7bd071b646d4a93efaee19ba0fb71e47ed730ae
Matheus Neves <matheusneves1989@gmail.com>
2020-06-01 16:28:47 -0300

    feat(ratelimit): add middleware rateLimiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -16,6 +17,7 @@ import routes from './routes';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
